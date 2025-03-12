bd9bc9f88e7cdae08daccb9bf0e1e513329774bb
Daniel Krupnyi <daniel.krupnyi@gmail.com>
2025-03-12 07:37:42 +0000

    disabled eslint rule no-unescaped-entities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ const eslintConfig = [
 		rules: {
 			'@typescript-eslint/no-unused-vars': 'off',
 			'react-hooks/exhaustive-deps': 'off',
+			'react/no-unescaped-entities': 'off',
 		},
 	}),
 ];
