ce2196991e87eae6a6fe70f394aed92a41a69861
Daniel Krupnyi <daniel.krupnyi@gmail.com>
2025-03-12 15:14:03 +0000

    test blog page

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -1,17 +1,16 @@
-import { getBlogPageData, getPosts } from '@/actions';
 import { Container } from '@/components/container';
-import { PostsGrid } from '@/components/posts-grid';
-import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
-import Link from 'next/link';
 
 const BlogPage = async () => {
-	const pageData = await getBlogPageData();
-	const posts = await getPosts();
+	// const pageData = await getBlogPageData();
+	// const posts = await getPosts();
 
 	return (
 		<Container variant='secondary'>
-			<div className='py-8 space-y-8'>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+			voluptatibus id ipsa consequatur? Porro aliquam repellendus, iste ad iusto
+			adipisci placeat? Illo dolorem nemo sit voluptates repellat, neque maxime
+			laboriosam.
+			{/* <div className='py-8 space-y-8'>
 				<div className='space-y-2'>
 					<h1 className='text-3xl font-bold'>{pageData?.title}</h1>
 					<p className='text-muted-foreground'>{pageData?.subtitle}</p>
@@ -25,7 +24,7 @@ const BlogPage = async () => {
 						</Link>
 					</Button>
 				</div>
-			</div>
+			</div> */}
 		</Container>
 	);
 };
